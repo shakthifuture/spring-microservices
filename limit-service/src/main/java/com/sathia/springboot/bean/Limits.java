1641468b0d43549e72dceead20fdf01378f3c1b7
Sathia <smuthu@archsystemsinc.com>
2021-10-30 20:35:24 +0530

    Moved all the services in the repo

---
+++ limit-service/src/main/java/com/sathia/springboot/bean/Limits.java
@@ -0,0 +1,15 @@
+package com.sathia.springboot.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Limits {
+	
+	private int minimum;
+	private int maximum;
+
+}
